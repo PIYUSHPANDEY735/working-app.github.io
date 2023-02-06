1c02a89e92090da82e21382bd07aa24e5c2cea36
User_without_data <122740251+PIYUSHPANDEY735@users.noreply.github.com>
2023-02-06 21:36:49 +0530

    Update Connection.php
    Signed-off-by: User_without_data <122740251+PIYUSHPANDEY735@users.noreply.github.com>

--- Connection.php
+++ Connection.php
@@ -1,7 +1,7 @@
 <?php
-$host = "sql12.freesqldatabase.com";
-$user = "sql12595992";
-$password = "piyushdatabase";
+$host = "freesqldatabase.com";
+$user = "sql45357777";
+$password = "merapassword";
 $db_name = "sql12595992";
 $clean = mysqli_connect($host, $user, $password, $db_name,);
 if (mysqli_connect_errno()) {
